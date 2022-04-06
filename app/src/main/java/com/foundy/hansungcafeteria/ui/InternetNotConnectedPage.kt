4524ab09ce0a69b4da4dfaf9ee0b647c1bc73883
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2022-04-07 02:27:50 +0900

    Update InternetNotConnectedPage.kt

--- app/src/main/java/com/foundy/hansungcafeteria/ui/InternetNotConnectedPage.kt
+++ app/src/main/java/com/foundy/hansungcafeteria/ui/InternetNotConnectedPage.kt
@@ -1,25 +1,41 @@
 package com.foundy.hansungcafeteria.ui
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Warning
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun InternetNotConnectedPage(onClickRefresh: () -> Unit) {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "인터넷에 연결되지 않았습니다.")
+        Icon(
+            Icons.Rounded.Warning,
+            contentDescription = "인터넷",
+            tint = MaterialTheme.colors.onBackground.copy(alpha = 0.3F),
+            modifier = Modifier
+                .padding(bottom = 16.dp)
+                .size(60.dp)
+        )
+        Text(text = "인터넷에 연결할 수 없습니다.")
         TextButton(onClick = onClickRefresh) {
             Text("새로고침")
         }
     }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun InternetNotConnectedPagePreview() {
     InternetNotConnectedPage(onClickRefresh = {})
